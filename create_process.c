d371c724120b89aa9e72465888f5a6c2da8f3ae9
Camilo Moncada <monk-da@hotmail.com>
2020-04-14 17:32:23 -0500

    function for excecute a process created

---
+++ create_process.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * create_process - This function create a child process and executes
+ * a specific program.
+ * @av: Pointer to an array of strings with all arguments of input buffer
+ * in each postiion.
+ * Return: void
+ **/
+int create_process(char *av[])
+{
+	int count = 1;
+	char *full_path = NULL, *full_file = NULL, error_msg[100];
+
+	sprintf(error_msg, "%s: %d: %s: not found\n", av[0], count, av[1]);
+	full_path = _getenv("PATH");
+	full_file = validate_file(full_path, av[1]);
+	free(full_path);
+	if ((execve(full_file, (av + 1), environ)) == -1)
+	{
+		free(full_file);
+
+		write(2, error_msg, strlen(error_msg));
+		exit(127);
+	}
+	return (0);
+}
