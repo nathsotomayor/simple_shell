b491265e5e7843fa538e62bb5c92406be09c8e1f
Camilo Moncada <monk-da@hotmail.com>
2020-04-14 17:30:54 -0500

    function for get variable envirionment

---
+++ _getenv.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * _getenv - get variable enviriomment
+ * @name: name the variable envirionment
+ * Return: char value envirionment
+ **/
+char *_getenv(char *name)
+{
+	char *token1 = NULL, *token2 = NULL;
+	char *env_name = NULL, *current_env = NULL;
+	int i = 0;
+
+	env_name = _strdup(name);
+
+	while (environ[i] && environ)
+	{
+		current_env = NULL;
+		token2 = NULL;
+		current_env = _strdup(environ[i]);
+		token1 = strtok(current_env, "=");
+		token2 = _strdup(strtok(NULL, "="));
+		if (_strcmp(env_name, token1) == 0)
+		{
+			break;
+		}
+		free(current_env);
+		free(token2);
+		i++;
+	}
+	free(current_env);
+	current_env = NULL;
+	free(env_name);
+	return (token2);
+}
