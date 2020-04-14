253e557f881292e030fc97e11bf4d7ca68aa0e52
Juan Sebastian <juanllano93@gmail.com>
2020-04-14 23:45:09 +0000

    add function main

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * main - shell proyect
+ * @ac: count arguments
+ * @av: store arguments
+ * @env: variable envirionment system
+ * Return: estatus_exit value;
+ **/
+
+int main(int ac, char *av[], char **env)
+{
+	int count_exe = 1, status_exit = 0;
+	(void)ac;
+
+	if (ac > 1)
+	{
+		non_interact(av);
+		return (0);
+	}
+	else
+	{
+		status_exit = interactive(av, &count_exe, env);
+		return (status_exit);
+	}
+	return (0);
+}
