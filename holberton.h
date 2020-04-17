142cb90e6c291b3ba907e0f437462c42980b0838
Camilo Moncada <monk-da@hotmail.com>
2020-04-16 19:39:15 -0500

    update prototypes

--- holberton.h
+++ holberton.h
@@ -14,32 +14,32 @@
 #include <signal.h>
 
 /* Shell functions shell_functions.c*/
-	int non_interact(char **av);
-	int interactive(char *av[], int count_exe, char **env);
-	int create_process(char *av[], int count_exe, char **env);
-	char *_getenv(char *name, char **env);
-	int print_env(char **env);
-	void _sighandler(int singint);
-	int e_xit(char *line, int status, char *av, int count);
-	int _atoi(char *arr);
+int non_interact(char **av);
+int interactive(char *av[], int count_exe, char **env);
+int create_process(char *av[], int count_exe, char **env);
+char *_getenv(char *name, char **env);
+int print_env(char **env);
+void _sighandler(int singint);
+int e_xit(char *line, int status, char *av, int count);
+int _atoi(char *arr);
 
 /* funcions search_path path_fuctions.c*/
-	char *validate_file(char *full_path, char *file);
+char *validate_file(char *full_path, char *file);
 
 
-	/* Str functions  str_functions.c*/
-	char *_strcpy(char *dest, char *src);
-	int _strlen(const char *s);
-	char *_strcat(char *dest, char *src);
-	char *_strdup(char *str);
+/* Str functions  str_functions.c*/
+char *_strcpy(char *dest, char *src);
+int _strlen(const char *s);
+char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
 
-	/* Str functions  strcmp_functions.c*/
-	int _strcmp(char *s1, char *s2);
-	int _strncmp(const char *s1, const char *s2, size_t n);
+/* Str functions  strcmp_functions.c*/
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
 
-	/* Memory allocate functions */
-	char *_memset(char *s, char b, unsigned int n);
-	void *_calloc(unsigned int nmemb, int size);
-	unsigned int lenght_array(char **p);
+/* Memory allocate functions */
+char *_memset(char *s, char b, unsigned int n);
+void *_calloc(unsigned int nmemb, int size);
+unsigned int lenght_array(char **p);
 
 #endif
